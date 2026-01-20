cd36e94080103e600b5746a339cd592905931d1a
Louis Chatain <louischatain123@gmail.com>
2026-01-20 03:48:20 +0100

    put synchronus function into fastapi built in run_in_threadpool

--- router/oauth2.py
+++ router/oauth2.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.concurrency import run_in_threadpool
 from werkzeug.security import check_password_hash
 from datetime import datetime, timedelta
 from typing import Optional
@@ -33,71 +34,57 @@ def create_access_token(data, expires_delta: Optional[timedelta] = None) -> str:
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-
+http_client = httpx.AsyncClient()
 # --- LOGIN ENDPOINT ---
 @router.post(
     "/login",
-    deprecated=False,
-    name='login',
     summary="Create a token for a user.",
     description="",
-    response_model=None,
-    status_code=status.HTTP_200_OK,
-    response_description="User token is issued",
-    responses={
-        200: {
-            "description": "SUCCESS - The token was created!",
-        }
-    }
 )
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()) -> dict[str, str]:
     """
     1. Receive username/password.
     2. Call User Service to get the stored user (and hash).
     3. Verify password.
     4. Issue Token.
     """
-
     
-    async with httpx.AsyncClient() as client:
-        try:
-            if not SECRET_KEY:
-                raise ValueError("CRITICAL: SECRET_KEY environment variable is required.")
-            
-            # We use the service name 'user-api' which Docker resolves automatically
-            response = await client.get(
-                "http://user-api/user/read_user_by_username",    #"http://user-api:8000/user/read_user_by_username" when not using kubernetes
-                params={"username": form_data.username},
-                headers={"x-internal-token": SECRET_KEY}
-            )
-            # This will jump straight to the 'except httpx.HTTPStatusError' block if status is not 2xx
-            response.raise_for_status()
+    if not SECRET_KEY:
+        raise ValueError("CRITICAL: SECRET_KEY environment variable is required.")
+    try:
+        response = await http_client.get(
+            "http://user-api/user/read_user_by_username",    #"http://user-api:8000/user/read_user_by_username" when not using kubernetes
+            params={"username": form_data.username},
+            headers={"x-internal-token": SECRET_KEY}
+        )
+        # This will jump straight to the 'except httpx.HTTPStatusError' block if status is not 2xx
+        response.raise_for_status()
 
-        except httpx.RequestError as e:
-            # Happens if the service is totally down or DNS fails
+    except httpx.RequestError as e:
+        # Happens if the service is totally down or DNS fails
+        raise HTTPException(
+            status_code=503, 
+            detail=f"User Service unreachable: {e}"
+        )
+    except httpx.HTTPStatusError as e:
+        # Happens if service replied with 401, 404, 500, etc.
+        if e.response.status_code == 401:
             raise HTTPException(
-                status_code=503, 
-                detail=f"User Service unreachable: {e}"
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail=f"Incorrect username or password: {e}",
+                headers={"WWW-Authenticate": "Bearer"},
             )
-
-        except httpx.HTTPStatusError as e:
-            # Happens if service replied with 401, 404, 500, etc.
-            if e.response.status_code == 401:
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail=f"Incorrect username or password: {e}",
-                    headers={"WWW-Authenticate": "Bearer"},
-                )
-            # Fallback for other status errors (like 404 or 500)
-            raise HTTPException(
-                status_code=e.response.status_code, 
-                detail=f"500 error: {e}"
-            )
+        # Fallback for other status errors (like 404 or 500)
+        raise HTTPException(
+            status_code=e.response.status_code, 
+            detail=f"500 error: {e}"
+        )
 
     user_data = response.json()
 
-    
-    if not check_password_hash(user_data["hashed_password"], form_data.password):
+    is_valid = await run_in_threadpool(check_password_hash, user_data["hashed_password"], form_data.password)
+
+    if not is_valid:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
